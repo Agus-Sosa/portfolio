3ee698f81e863e0e21bb23d7e3b1ff861f20d695
Agus-Sosa <sosaagustin351@gmail.com>
2023-03-09 18:07:19 -0300

    porfolio

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,25 +1,82 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import '../styles/header.css'
+import { useState } from 'react'
 import ScrollToTop from 'react-scroll-to-top'
+import Offcanvas from 'react-bootstrap/Offcanvas';
+import Button from 'react-bootstrap/Button';
 // import NavbarSection from 'react-daisyui/dist/Navbar/NavbarSection'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
-const Header = () => {
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
+
+
+const Header = ({ name, ...props }) => {
+    const [show, setShow] = useState(false);
+    const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
     return (
         <>
-        <section className='contenedor-header'>
+        <header className='contenedor-header'>
         <div className="contenedor-nombre">
         <h3>Agustin Sosa</h3>
         </div>
-        <div className='contenedor-links'>
+        {/* <div className='contenedor-links'> */}
             <nav>
                 <Link to={'Sobremi'}><span>Sobre mi</span></Link>
                 <Link to={'Proyectos'}><span>Proyectos</span></Link>
                 <Link to={'Contactos'}><span>Contacto</span></Link>
             </nav>
+        {/* </div> */}
+        <div className="contenedor-slide" color='#E5BA73'>
+        {/* <Button color='#E5BA73' onClick={handleShow} className="me-2">
+        {name}
+        Menu
+      </Button>
+      <Offcanvas show={show} onHide={handleClose} {...props}>
+        <Offcanvas.Header closeButton>
+          <Offcanvas.Title>Offcanvas</Offcanvas.Title>
+        </Offcanvas.Header>
+        <Offcanvas.Body>
+          Some text as placeholder. In real life you can have the elements you
+          have chosen. Like, text, images, lists, etc.
+        </Offcanvas.Body>
+      </Offcanvas> */}
+      {['sm'].map((expand) => (
+            <Navbar key={expand} bg="light" expand={expand} className="mb-3">
+            <Container fluid>
+                <Navbar.Brand href="#"></Navbar.Brand>
+                <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} />
+                <Navbar.Offcanvas
+                id={`offcanvasNavbar-expand-${expand}`}
+                aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
+                placement="top"
+                >
+                <Offcanvas.Header closeButton>
+                    <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${expand}`}>
+                    Agustin Sosa
+                    </Offcanvas.Title>
+                </Offcanvas.Header>
+                <Offcanvas.Body>
+                    <Nav className="justify-content-end flex-grow-1 pe-3">
+                    <Nav.Link href="#action1">Acerca de mi</Nav.Link>
+                    <Nav.Link href="#action2">Proyectos</Nav.Link>
+                    <Nav.Link href='#action3'> Contacto</Nav.Link>
+                    </Nav>
+                    
+                </Offcanvas.Body>
+                </Navbar.Offcanvas>
+            </Container>
+            </Navbar>
+        ))}
         </div>
-        </section>
+        </header>
         <div className="contenedor-scroll-top">
         <ScrollToTop smooth className='scroll-top'/>
 
