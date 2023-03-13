50f02c15de8381b3dd605855c7944897e7cc557c
Agus-Sosa <sosaagustin351@gmail.com>
2023-03-13 01:17:52 -0300

    portfolio

--- src/components/Contacto.jsx
+++ src/components/Contacto.jsx
@@ -8,6 +8,7 @@ const Contacto = () => {
             <h2>Contacto</h2>
             <a href="https://www.linkedin.com/in/agust%C3%ADn-sosa-a6b29b248/">Linkedin</a>
             <a href="mailto:sosaagustin351@gmail.com?Subject=Aquí%20el%20asunto%20del%20email">Email</a>
+            <a href="https://github.com/Agus-Sosa">GitHub</a>
         </section>
         </div>
         </>
