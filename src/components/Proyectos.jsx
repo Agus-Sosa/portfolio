2bd1a7ba1670b7a5446ec2f6b332cb03bfbf8e3d
Agus-Sosa <sosaagustin351@gmail.com>
2023-03-31 17:13:01 -0300

    Mi portfolio

--- src/components/Proyectos.jsx
+++ src/components/Proyectos.jsx
@@ -3,6 +3,7 @@ import '../styles/proyectos.css'
 import dwCaptura from './assets/proyecto-dw.png'
 import jsCaptura from './assets/proyecto-js.png'
 import reactCaptura from './assets/proyecto-react.png'
+import {BsArrowLeftShort, BsArrowRightShort} from 'react-icons/bs'
 // import reactCaptura2 from './assets/proyecto-react-2.png'
 const Proyectos = () => {
   return (
@@ -22,12 +23,14 @@ const Proyectos = () => {
         <h2>Front End</h2>
         <section className="contenedor-proyecto" id='proyecto-dw'>
         <h3>Desarrollo Web</h3>
-        <a href="https://github.com/Agus-Sosa/proyecto-desarrollo-web-Agustin-Sosa">
         <img 
-        // src='./assets/Afterlife.png'
+        
         src={dwCaptura}
         alt="" />
-        </a>
+        <div className='contenedor-ver-proyecto'>
+            <a href="https://github.com/Agus-Sosa/proyecto-desarrollo-web-Agustin-Sosa" target={'_blank'}><BsArrowLeftShort/>Ver GitHub</a>
+            <a href="https://afterlifestore.netlify.app/index.html" target={'_blank'}>Ver Sitio<BsArrowRightShort/></a>
+            </div>
         <p>Primer proyecto que hice como desarrollador Front End, utilizando lo basico para crear sitios web.</p>
         <p>El proyecto que realice fue un Ecommerce de Videojuegos, utilizando lo basico (HTML, CSS)</p>
         <div className="contenedor-herramientas">
@@ -38,11 +41,13 @@ const Proyectos = () => {
         <div className="division"></div>
         <section className="contenedor-proyecto" id='proyecto-js'>
           <h3>Javascript</h3>
-          <a href="https://github.com/Agus-Sosa/Proyecto-javascript-Agustin-Sosa">
             <img 
             src={jsCaptura}
             alt="" />
-          </a>
+            <div className='contenedor-ver-proyecto'>
+            <a href="https://github.com/Agus-Sosa/Proyecto-javascript-Agustin-Sosa" target={'_blank'}><BsArrowLeftShort/>Ver GitHub</a>
+            <a href="https://yoshuanime.netlify.app/" target={'_blank'}>Ver Sitio<BsArrowRightShort/></a>
+            </div>
           <p>Segundo Proyecto que realice en el segundo curso, utilizando Javascript vanilla.</p>
           <p>El Segundo proyecto realice fue una web dedicada a series, utilizando Javascript vanilla, hice la creacion del apartado de favoritos y tambien el apartado de las suscripciones </p>
           <div className="contenedor-herramientas">
@@ -53,11 +58,13 @@ const Proyectos = () => {
         <div className="division"></div>
         <section className="contenedor-proyecto" id='proyecto-react'>
           <h3>React Js</h3>
-          <a href="">
             <img 
             src={reactCaptura}
             alt="" />
-          </a>
+            <div className='contenedor-ver-proyecto'>
+            <a href="https://github.com/Agus-Sosa/Proyecto-React-Agustin-Sosa" target={'_blank'}><BsArrowLeftShort/>Ver GitHub</a>
+            <a href="https://proyecto-react-agustin-sosa.vercel.app/" target={'_blank'}>Ver Sitio<BsArrowRightShort/></a>
+            </div>
           <p>Tercer Proyecto que realice en el ultimo curso de Desarrollador Front End, React JS</p>
           <p>Lo realizado en este proyecto fue crear un sitio web Ecommerce, con metodo de pagos y extraer datos de los productos desde una base de datos (FireBase).
             En la misma utilice bastantes librerias para mejorar la experiencia de la pagina 
